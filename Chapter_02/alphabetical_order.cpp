02d27d006a11f7752e9997b2352bd713bb9a3cd5
k56395888-source <k56395888@gmail.com>
2026-09-09 18:03:32 +0300

    Update alphabetical_order.cpp

--- Chapter_02/alphabetical_order.cpp
+++ Chapter_02/alphabetical_order.cpp
@@ -1,3 +1,32 @@
 // Rewrite the ascending.cpp program to process stings instead.
 // So, if the user enters the values Electrical Eng., Computer Eng., Biomedical Eng.,
 // the output should be Biomedical Eng., Computer Eng., Electrical Eng..
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string a, b, c;
+
+    cout << "Enter first engineering field: ";
+    getline(cin, a);
+
+    cout << "Enter second engineering field: ";
+    getline(cin, b);
+
+    cout << "Enter third engineering field: ";
+    getline(cin, c);
+
+    // Sort the strings in ascending order
+    if (a > b) swap(a, b);
+    if (a > c) swap(a, c);
+    if (b > c) swap(b, c);
+
+    cout << "Engineering fields in ascending order:\n";
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+
+    return 0;
+}
